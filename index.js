154204be30df5a1a191e4a6398c91f90de2fe37a
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-01-10 22:29:06 +0100

    Delete empty lines

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var OSinfo = require('./modules/OSinfo');
+
 process.stdin.setEncoding('utf-8');
-
 console.log('Hello, give the command!')
-
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();
 	if(input !== null) {
